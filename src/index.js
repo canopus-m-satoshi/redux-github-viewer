9ded2f90cf629633ca39a09d9166b75b5650704d
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-09-13 16:51:32 +0900

    index.jsのReset CSSをコメントアウト

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Reset />
+      {/* <Reset /> */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
