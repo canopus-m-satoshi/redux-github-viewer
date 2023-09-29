ac29960688392fd7fcf95a67382cc58152fe475f
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-09-30 01:55:47 +0900

    consoleエラー修正対応途中

--- src/store.js
+++ src/store.js
@@ -4,6 +4,13 @@ import uiReducer from './features/ui/uiSlice'
 import issueReducer from './features/issue/issueSlice'
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      // serializableCheck: {
+      //   ignoredActions: ['ui/push', 'ui/show'],
+      // },
+      // serializableCheck: false,
+    }),
   reducer: {
     toggleTab: toggleTabReducer,
     ui: uiReducer,
