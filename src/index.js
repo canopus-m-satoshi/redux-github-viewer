ba06e4767abbd0662fa6a088bada91f7b9d807ee
Satoshi Matsushima <work.satoshi1993@gmail.com>
2023-10-09 15:50:38 +0900

    gh-pagesの追加

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { createGlobalStyle } from 'styled-components'
@@ -36,11 +36,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 )
 
